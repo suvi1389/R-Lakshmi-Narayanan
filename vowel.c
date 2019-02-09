896d2ee8c991253f6e00b40c6fe2c62ec7862f7d
suvi1389 <47452702+suvi1389@users.noreply.github.com>
2019-02-09 15:57:18 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -7,7 +7,7 @@ int main(void) {
   {
     if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' || ch == 'u' || ch == 'U')
   {
-    printf("vowel");
+    printf("Vowel");
   }
   else
   {
